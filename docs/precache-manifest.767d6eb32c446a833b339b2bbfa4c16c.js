98d7829932a4d4c417d6e8843e16e36cb68b1299
Emiliano López <emiliano030593@gmail.com>
2020-07-15 20:12:27 -0500

    Add Docs

---
+++ docs/precache-manifest.767d6eb32c446a833b339b2bbfa4c16c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16586240ef5b8a323808792b734942f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c68b6157fde7ee4ba97a",
+    "url": "/static/css/main.8daff081.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c68b6157fde7ee4ba97a",
+    "url": "/static/js/main.67a3b754.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
