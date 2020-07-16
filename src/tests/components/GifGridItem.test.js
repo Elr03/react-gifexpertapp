2bd01c8b40a46c9e449d4b43430644282a0cd3b2
Emiliano López <emiliano030593@gmail.com>
2020-07-16 10:44:26 -0500

    first test component

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,11 +3,33 @@ import React from 'react'
 import { shallow } from 'enzyme';
 import { GifGridItem } from '../../components/GifGridItem';
 
-describe('Pruebas en <GifGridItem />', () => {
+describe('Test en <GifGridItem />', () => {
+    const title = 'Un título';
+    const url = 'https://localhost/algo.jpg';
+    const wrapper = shallow(<GifGridItem title={title} url={url}/>)
+
     test('should show correct component', () => {
-        const wrapper = shallow(<GifGridItem />)
         expect(wrapper).toMatchSnapshot();
     })
+    
+    test('should have a phargraph with a title', () => {
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+    })
+
+    test('should hava a image equal to props', () => {
+        const img = wrapper.find('img');
+        //console.log(img.prop('src'))
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    })
+
+    test('should have animate_fadeIn', () => {
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+        expect(className.includes('animate__fadeIn')).toBe(true);
+    })
+    
     
     
 })
